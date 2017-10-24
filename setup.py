3edd2e409ff2a4966f364bedce84f1e178bdbad4
Jens <jensoen@gmail.com>
2017-10-24 19:24:19 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ihcsdk',
+    version='1.0.2',
+    description='IHC Python SDK',
+    long_description='SDK for connection to the LK IHC Controller. (Made for interfacing to home assistant)',
+    author='Jens Ø. Nielsen',
+    url='https://github.com/dingusdk/PythonIhcSdk',
+    packages=['ihcsdk'],
+    license='GPL-3.0',
+)
