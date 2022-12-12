9272cc75bb002e6bbbd094abf399baabe96b412b
Jens <jensoen@gmail.com>
2022-12-12 16:55:57 +0100

    Bump to version 2.8.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="ihcsdk",
-    version="2.8.2",
+    version="2.8.3",
     description="IHC Python SDK",
     long_description=(
         "SDK for connection to the LK IHC Controller. "
