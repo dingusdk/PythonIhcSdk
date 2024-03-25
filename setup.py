6cbb7cffd5293a8769053b6b7d48b941acadf734
Jens Østergaard Nielsen <jensoen@gmail.com>
2024-03-25 11:21:47 +0100

    Update to 2.8.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="ihcsdk",
-    version="2.8.5",
+    version="2.8.6",
     description="IHC Python SDK",
     long_description=(
         "SDK for connection to the LK IHC Controller. "
